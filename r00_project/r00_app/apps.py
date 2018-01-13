6eea2fe8694d35ddd1c48d99728530dd65503a50
Mickael SORIN <msorin@e2r10p8.42.fr>
2018-01-13 22:53:43 +0100

    premiere config avec preset html

---
+++ r00_project/r00_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class R00AppConfig(AppConfig):
+    name = 'r00_app'
---
+++ r00_project/r00_app/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def titlescreen(request):
+	return render(request, "r00/titlescreen.html")
+
+def worldmap(request):
+	return render(request, "r00/worldmap.html")
+
+def battle(request):
+	return render(request, "r00/battle.html")
+
+def moviedex(request):
+	return render(request, "r00/moviedex.html")
+
+def detail(request):
+	return render(request, "r00/detail.html")
+
+def option(request):
+	return render(request, "r00/option.html")
+
+def save(request):
+	return render(request, "r00/save.html")
+
+def load(request):
+	return render(request, "r00/load.html")
